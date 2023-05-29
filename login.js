c60a591e713bf5d2a0b2ea27c9a861ec590b8dfb
Siddarth <116632503+sid1974@users.noreply.github.com>
2023-05-29 14:40:20 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,5 @@
+var email=document.div['mail-error'];
+var pass=document.div['pass-error']; 
+
+var mail-error=document.getElementById('mail-error');
+var pass-error=document.getElementById('pass-error');
